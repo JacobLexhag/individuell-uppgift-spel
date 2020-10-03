6c9cd18a76c8730bb17fa9b2321e560f4a32f24c
JacobLehxag <jacob.lexhag19@gmail.com>
2020-10-03 15:18:06 +0200

    fortsatt med players och har nu fått det att funka.
    började lite med meny av val.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,49 +1,89 @@
 package com.company;
 import java.util.*;
-public class Game  {
-
+public class Game {
 
 
     Scanner input = new Scanner(System.in);
-    int checkRound;
     ArrayList<Player> allPlayers = new ArrayList<Player>();
+    int checkRound;
+    int chooseHowManyPlayers;
 
 
     public Game() {
         System.out.println("Welcome to a farm simulator game");
         checkRounds();
+        createPlayers();
+        playGame();
     }
 
-    public void playGame(Player player){
-        player.createPlayers();
+    public void playGame() {
 
-        for(var bög : allPlayers){
-            System.out.println(bög);
+        choiceMenu();
+
+    }
+
+    public void createPlayers() {
+        System.out.println("\nChoose how many players who are going to play 1-4");
+        chooseHowManyPlayers = input.nextInt();
+        if (chooseHowManyPlayers == 1) {
+            System.out.println("Player 1 choose your name : ");
+
+            Player player1 = new Player(1000, input.next());
+            allPlayers.add(player1);
+        } else if (chooseHowManyPlayers == 2) {
+            System.out.println("Player 1 choose your name : ");
+            Player player1 = new Player(1000, input.next());
+            allPlayers.add(player1);
+            System.out.println("Player 2 choose your name : ");
+            Player player2 = new Player(1000, input.next());
+            allPlayers.add(player2);
+        } else if (chooseHowManyPlayers == 3) {
+            System.out.println("Player 1 choose your name : ");
+            Player player1 = new Player(1000, input.next());
+            allPlayers.add(player1);
+            System.out.println("Player 2 choose your name : ");
+            Player player2 = new Player(1000, input.next());
+            allPlayers.add(player2);
+            System.out.println("Player 3 choose your name : ");
+            Player player3 = new Player(1000, input.next());
+            allPlayers.add(player3);
+        } else {
+            System.out.println("Player 1 choose your name : ");
+            Player player1 = new Player(1000, input.next());
+            allPlayers.add(player1);
+            System.out.println("Player 2 choose your name : ");
+            Player player2 = new Player(1000, input.next());
+            allPlayers.add(player2);
+            System.out.println("Player 3 choose your name : ");
+            Player player3 = new Player(1000, input.next());
+            allPlayers.add(player3);
+            System.out.println("Player 4 choose your name : ");
+            Player player4 = new Player(1000, input.next());
+            allPlayers.add(player4);
         }
-
     }
 
+    public void checkRounds() {
+        System.out.println("\nChoose how many rounds you wanna play 5-30");
+        checkRound = input.nextInt();
 
-
-        public void checkRounds () {
-            System.out.println("\nChoose how many rounds you wanna play 5-30");
+        while (!(checkRound >= 5 && checkRound <= 30)) {
+            System.out.println("Try again input a number between 5 and 30 : ");
             checkRound = input.nextInt();
 
-            while (!(checkRound >= 5 && checkRound <= 30)) {
-                System.out.println("Try again input a number between 5 and 30 : ");
-                checkRound = input.nextInt();
-
-            }
-
         }
 
-        public void choiceMenu(){
-            if (input.nextLine().equals("buy")){
-                switch (input.nextLine()) {
-                    case "pig" -> System.out.println("hello");
-                    case "horse"-> System.out.println("fuck off");
-                }
+    }
+
+    public void choiceMenu() {
+        System.out.println(allPlayers.get(0).playerName + " " + allPlayers.get(0).money);
+        if (input.next().equals("buy")) {
+            switch (input.next()) {
+                case "pig" -> System.out.println("hello");
+                case "horse" -> System.out.println("najs");
             }
         }
 
+
+    }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -3,12 +3,10 @@ package com.company;
 import java.util.*;
 
 public class Player {
-    public ArrayList<Player> allPlayers = new ArrayList<Player>();
-    Scanner input = new Scanner(System.in);
-    int chooseHowManyPlayers;
+
     public String playerName;
     public int money;
-    public Player player1;
+
 
 
     public Player(int money , String playerName){
@@ -16,50 +14,10 @@ public class Player {
     this.playerName = playerName;
 
     }
-    public void createPlayers(){
-        System.out.println("\nChoose how many players who are going to play 1-4");
-        chooseHowManyPlayers = input.nextInt();
-        if (chooseHowManyPlayers == 1) {
-            System.out.println("Player 1 choose your name : ");
-
-            player1 = new Player(1000, input.next());
-            allPlayers.add(player1);
-        }
-        else if (chooseHowManyPlayers == 2) {
-            System.out.println("Player 1 choose your name : ");
-            Player player1 = new Player(1000, input.next());
-            allPlayers.add(player1);
-            System.out.println("Player 2 choose your name : ");
-            Player player2 = new Player(1000, input.next());
-            allPlayers.add(player2);
-        }
-        else if (chooseHowManyPlayers == 3) {
-            System.out.println("Player 1 choose your name : ");
-            Player player1 = new Player(1000, input.next());
-            allPlayers.add(player1);
-            System.out.println("Player 2 choose your name : ");
-            Player player2 = new Player(1000, input.next());
-            allPlayers.add(player2);
-            System.out.println("Player 3 choose your name : ");
-            Player player3 = new Player(1000, input.next());
-            allPlayers.add(player3);
-        }
-        else{
-            System.out.println("Player 1 choose your name : ");
-            Player player1 = new Player(1000, input.next());
-            allPlayers.add(player1);
-            System.out.println("Player 2 choose your name : ");
-            Player player2 = new Player(1000, input.next());
-            allPlayers.add(player2);
-            System.out.println("Player 3 choose your name : ");
-            Player player3 = new Player(1000, input.next());
-            allPlayers.add(player3);
-            System.out.println("Player 4 choose your name : ");
-            Player player4 = new Player(1000, input.next());
-            allPlayers.add(player4);
-        }
-    }
-
 
 
 }
+
+
+
+
